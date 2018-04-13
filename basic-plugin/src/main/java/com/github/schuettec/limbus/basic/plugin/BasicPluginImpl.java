64f7edbc66f071136786494f012646cabc62fb6c
schuettec <schuettec@DESKTOP-LPBFIEM.fritz.box>
2018-04-13 11:34:16 +0200

    Basic Plugin Implementation prepared

---
+++ basic-plugin/src/main/java/com/github/schuettec/limbus/basic/plugin/BasicPluginImpl.java
@@ -0,0 +1,43 @@
+package com.github.schuettec.limbus.basic.plugin;
+
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import com.github.schuettec.limbus.basic.BasicPluginInterface;
+
+/**
+ * This is the implementation of a basic plugin implementation.
+ * 
+ * @author schuettec
+ *
+ */
+public class BasicPluginImpl implements BasicPluginInterface {
+
+	@Override
+	public void initialize() throws Exception {
+	}
+
+	@Override
+	public void finish() {
+	}
+
+	@Override
+	public Set<String> readSystemProperties() {
+		Properties properties = System.getProperties();
+		Set<Object> keySet = properties.keySet();
+		Set<String> propertySet = new HashSet<>();
+		for (Object key : keySet) {
+			Object value = properties.get(key);
+			String string = new StringBuilder().append(key).append(value).toString();
+			propertySet.add(string);
+		}
+		return propertySet;
+	}
+
+	@Override
+	public void trySystemExit() {
+		System.exit(0);
+	}
+
+}
