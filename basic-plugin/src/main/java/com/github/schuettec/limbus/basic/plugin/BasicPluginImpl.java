3771961342de00b2d5b979fc244212793cdce175
schuettec <schuettec@DESKTOP-LPBFIEM.fritz.box>
2018-04-18 19:23:35 +0200

    Demonstrating package access
    - Work directory to .gitignore

--- basic-plugin/src/main/java/com/github/schuettec/limbus/basic/plugin/BasicPluginImpl.java
+++ basic-plugin/src/main/java/com/github/schuettec/limbus/basic/plugin/BasicPluginImpl.java
@@ -14,30 +14,41 @@ import com.github.schuettec.limbus.basic.BasicPluginInterface;
  */
 public class BasicPluginImpl implements BasicPluginInterface {
 
-	@Override
-	public void initialize() throws Exception {
-	}
+  @Override
+  public void initialize() throws Exception {
+    // The following lines demonstrate that the plugin is able to access classes in the package defined by the class
+    // overriding LimbusEngineImpl.
+    ClassLoader pluginClassLoader = getClass().getClassLoader();
+    Class<?> hostApp = pluginClassLoader.loadClass("com.github.schuettec.limbus.basic.BasicHostApplication");
+    Class<?> launcher = pluginClassLoader.loadClass("com.github.schuettec.limbus.basic.BasicHostLauncher");
+    System.out.println("Got access to the following classes:");
+    System.out.println("- " + hostApp.toString());
+    System.out.println("- " + launcher.toString());
+  }
 
-	@Override
-	public void finish() {
-	}
+  @Override
+  public void finish() {
+  }
 
-	@Override
-	public Set<String> readSystemProperties() {
-		Properties properties = System.getProperties();
-		Set<Object> keySet = properties.keySet();
-		Set<String> propertySet = new HashSet<>();
-		for (Object key : keySet) {
-			Object value = properties.get(key);
-			String string = new StringBuilder().append(key).append(" = ").append(value).toString();
-			propertySet.add(string);
-		}
-		return propertySet;
-	}
+  @Override
+  public Set<String> readSystemProperties() {
+    Properties properties = System.getProperties();
+    Set<Object> keySet = properties.keySet();
+    Set<String> propertySet = new HashSet<>();
+    for (Object key : keySet) {
+      Object value = properties.get(key);
+      String string = new StringBuilder().append(key)
+          .append(" = ")
+          .append(value)
+          .toString();
+      propertySet.add(string);
+    }
+    return propertySet;
+  }
 
-	@Override
-	public void trySystemExit() {
-		System.exit(0);
-	}
+  @Override
+  public void trySystemExit() {
+    System.exit(0);
+  }
 
 }
